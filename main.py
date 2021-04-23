12dda93df0dd3c520a438cd88b717f9318f4ee87
Tubaher <38712907+Tubaher@users.noreply.github.com>
2021-04-23 16:01:07 -0500

    miss str

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from edit import edit_ds_transfer, edit_secondary_gie
 
 parser = argparse.ArgumentParser(description='Test of gender model in deepstream and its metrics')
 
-parser.add_argument('model_name', type=str, help='model name for testing)
+parser.add_argument('model_name', type=str, help='model name for testing')
 parser.add_argument('secondary', type=str, help='path to the secondary inference config file')
 parser.add_argument('video', type=str, help='path to the video for testing')
 parser.add_argument('ds_config', type=str, help='path to deepstream config file')
